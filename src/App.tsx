3bdab175b5ba42d4b0627943280463db33e1b6cf
TheAlex <143757165+AlexRz92@users.noreply.github.com>
2026-03-04 15:25:11 -0300

    Add onAddToCart prop to FeaturedProducts component

--- src/App.tsx
+++ src/App.tsx
@@ -247,6 +247,7 @@ function App() {
             products={featuredProducts}
             inventory={inventory}
             onProductClick={setSelectedProduct}
+            onAddToCart={addToCart}
           />
         )}
 
